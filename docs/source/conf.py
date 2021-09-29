9207bafd05f7e00547eef1846dcc5e8bbf163e33
MarieBergeret <pommepato@hotmail.fr>
2021-09-29 12:34:16 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Crédit forfait'
+copyright = '2021, Intence'
+author = 'Intence'
 
 release = '0.1'
-version = '0.1.0'
+version = '1.0'
 
 # -- General configuration
 
